f3fd14de2b481f99e51a4d49c50c81d96e460dbe
Juliet DiMenna <jdimenna@bayviewglen.ca>
2024-12-13 12:49:14 -0500

    server & db

---
+++ config/database.js
@@ -0,0 +1,22 @@
+import { MongoClient} from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const dbName = 'EMR';
+
+let db;
+
+export const connectDB = async () => {
+    if (db) return db;
+    try {
+        const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+        console.log("Connected to MongoDB");
+        db = client.db(dbName);
+        return db;
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+        throw error;
+    }
+};
