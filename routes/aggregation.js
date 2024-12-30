595a1dcc856617ba8842f43f2c38965ebdb1d400
Eric Tan <etan@bayviewglen.ca>
2024-12-29 23:55:00 -0500

    did my stuff

--- routes/aggregation.js
+++ routes/aggregation.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import connectDB from '../config/database.js';
-const router = express.router();
+import {connectDB} from '../config/database.js';
+import { ObjectId } from 'mongodb';
+const router = express.Router();
 
 router.get('/patients/aggregated-diagnosis', async(req,res) => {
     //most common diagnosis
@@ -86,5 +87,105 @@ router.get("/patients/prescribed-meds", async(req,res) => {
     ]).toArray();
     res.json(result);
 });
+
+router.get('/appointments/:id/details',async(req,res)=>{
+    try{
+        const db=await connectDB()
+        const result=await db.collection('appointments').aggregate([
+            {
+                $match:{
+                    _id:new ObjectId(req.params.id)
+                }                
+            },
+            {
+                $lookup:{
+                    from:'doctors',
+                    localField:'doctor_id',
+                    foreignField:'_id',
+                    as:'doctor_info'
+                }
+            },
+            {
+                $unwind:{
+                    path:'$doctor_info'
+                }
+            },
+            {
+                $lookup:{
+                    from:'patients',
+                    localField:'patient_id',
+                    foreignField:'_id',
+                    as:'patient_info'
+                }
+            },
+            {
+                $unwind:{
+                    path:'$patient_info'
+                }
+            },
+            {
+                $project:{
+                    _id:0,
+                    patient_id:0,
+                    doctor_id:0,
+                    'doctor_info._id':0,
+                    'patient_info._id':0
+                }
+            }
+        ]).toArray()
+        res.json(result)
+    }catch(error){
+        res.status(500).json({message:'failed to get appointment',error})
+    }
+})
+
+router.get('/doctors/:id/patients',async(req,res)=>{
+    try{
+        const db=await connectDB()
+        const result=await db.collection('appointments').aggregate([
+            {
+                $match:{
+                    doctor_id:new ObjectId(req.params.id)
+                }
+            },
+            {
+                $lookup:{
+                    from:'patients',
+                    localField:'patient_id',
+                    foreignField:'_id',
+                    as:'patients'
+                }
+            },
+            {
+                $unwind:{
+                    path:'$patients'
+                }
+            },
+            {
+                $addFields:{
+                    fullName:{
+                        $concat:[
+                            '$patients.first_name',' ','$patients.last_name'
+                        ]
+                    }
+                }
+            },
+            {
+                $group:{
+                    _id:null,
+                    patientNames:{$addToSet:'$fullName'}
+                }
+            },
+            {
+                $project:{
+                    _id:0
+                }
+            }
+        ]).toArray()
+        res.json(result)
+    }catch(error){
+        res.status(500).json({message:'failed to get doctor\'s patients',error})
+    }
+})
 
 export default router;
